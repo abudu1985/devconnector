99dc29edcaf9aa4d144c15c1417d09ab6538cdf6
igor <ig1985@ukr.net>
2018-10-16 11:41:03 +0300

    add authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   //mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector'
-  mongoURI: 'mongodb://igor:1Q2w3e4r!@ds233323.mlab.com:33323/devconnector'
+  mongoURI: 'mongodb://igor:1Q2w3e4r!@ds233323.mlab.com:33323/devconnector',
+  secretOrKey: 'secret'
 };
